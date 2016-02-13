d8737e27b56d52d358cafe61affa6b7c9dd23a74
Oskar Singer <oskarsinger@gmail.com>
2016-02-12 19:08:42 -0500

    Added convergence criterion

--- code/cca/app_grad.py
+++ code/cca/app_grad.py
@@ -1,6 +1,7 @@
 from utils.linalg import multi_dot
+from np.linalg import norm
 
-def get_cca_decomp(X, Y, eta1, eta2, epsilon):
+def get_cca_decomp(X, Y, eta1, eta2, epsilon1, epsilon2):
 
     (n1, p1) = X.shape
     (n2, p2) = Y.shape
@@ -23,7 +24,9 @@ def get_cca_decomp(X, Y, eta1, eta2, epsilon):
     Psi_t1 = np.randn(p2, p)
     unn_Psi_t1 = np.randn(p2, p)
 
-    while not converged: #should determine convergence criterion
+    converged = False
+
+    while not converged:
 
         unn_Phi_grad = np.dot(X.T, (np.dot(X, unn_Phi_t) - np.dot(Y, Psi_t)) ) / n
         unn_Phi_t1 = unn_Phi_t - eta1 * unn_Phi_grad
@@ -34,6 +37,11 @@ def get_cca_decomp(X, Y, eta1, eta2, epsilon):
         unn_Psi_t1 = unn_Psi_t - eta2 * unn_Psi_grad
         U_unn_Psi, s_unn_Psi, V_unn_Psi = get_svd_r(multi_dot([unn_Psi_t1.T, Sy, unn_Psi_t1]))
         Psi_t1 = multi_dot([unn_Psi_t1, U_unn_Psi, np.power(s_unn_Psi, -0.5), V_unn_Psi])
+
+        Phi_diff = norm(unn_Phi_t - unn_Phi_t1)
+        Psi_diff = norm(unn_Psi_t - unn_Psi_t1)
+
+        converged = Phi_diff < epsilon1 and Psi_diff < epsilon2
 
     return (Phi_t1, unn_Phi_t1, Psi_t1, unn_Psi_t1)
 
