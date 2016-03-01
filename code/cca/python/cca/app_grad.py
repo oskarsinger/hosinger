ee557c68a7cd915b7c31eac139d9714ca4032ead
Oskar Singer <oskarsinger@gmail.com>
2016-03-01 03:09:55 -0500

    Some small AppGrad updates

--- code/cca/python/cca/app_grad.py
+++ code/cca/python/cca/app_grad.py
@@ -1,32 +1,48 @@
 import numpy as np
 
-from numpy.random import randn
+from numpy.random import randn, choice
 from linal.utils import quadratic, get_svd_invert
 
-def get_batch_app_grad_decomp(X, Y, k, eta1, eta2, epsilon1, epsilon2):
+def get_batch_app_grad_decomp(X, Y, k, eta1, eta2, epsilon1, epsilon2, reg, batch_size=None):
 
     (n1, p1) = X.shape
     (n2, p2) = Y.shape
+    stochastic = False
 
     if not n1 == n2:
         raise ValueError(
             'Data matrices must have same number of data points.')
 
-    n = n1
-    p =  min([p1,p2])
+    if batch_size is not None:
+        if batch_size < k:
+            raise ValueError(
+                'Batch size must be greater than or equal to the value of k.')
+        else:
+            stochastic = True
+
+    Sx = None
+    Sy = None
 
     # Empirical second moment matrices for X and Y
-    Sx = np.dot(X.T, X) / n
-    Sy = np.dot(Y.T, Y) / n
+    if stochastic:
+        Sx = _get_minibatch_S(X, batch_size)
+        Sy = _get_minibatch_S(Y, batch_size)
+    else:
+        Sx = _get_regged_gram(X) / n1
+        Sy = _get_regged_gram(Y) / n1
 
     # Randomly initialize normalized and unnormalized canonical bases for 
     # timesteps t and t+1. Phi corresponds to X, and Psi to Y.
-    (Phi_t, unn_Phi_t, Psi_t, unn_Psi_t) = _init_bases(p, p1, p2)
+    (Phi_t, unn_Phi_t, Psi_t, unn_Psi_t) = _get_init_bases(Sx, Sy)
     (Phi_t1, unn_Phi_t1, Psi_t1, unn_Psi_t1) = (None, None, None, None)
 
     converged = False
 
     while not converged:
+
+        if stochastic:
+            Sx = _get_minibatch_S(X, batch_size)
+            Sy = _get_minibatch_S(Y, batch_size)
 
         # Get basis updates for both X and Y's canonical bases, normed and unnormed
         (unn_Phi_t1, Phi_t1) = _get_updated_bases(X, Y, unn_Phi_t, Psi_t, Sx, k, eta1)
@@ -40,6 +56,21 @@ def get_batch_app_grad_decomp(X, Y, k, eta1, eta2, epsilon1, epsilon2):
         (unn_Phi_t, Phi_t, unn_Psi_t, Psi_t) = (unn_Phi_t1, Phi_t1, unn_Psi_t1, Psi_t1)
 
     return (Phi_t, unn_Phi_t, Psi_t, unn_Psi_t)
+
+def _get_minibatch_S(A, batch_size, reg):
+
+    indexes = choice(np.arange(n1), replace=False, size=batch_size)
+    A_t = A[indexes,:]
+
+    return _get_regged_gram(A_t, reg) / batch_size
+
+def _get_regged_gram(A, reg):
+
+    p = A.shape[1]
+    gram = np.dot(A.T, A)
+    reg_matrix = np.diag(np.array([reg] * p))
+
+    return gram + reg
 
 def _is_converged(unnormed, unnormed_next, epsilon):
 
@@ -59,20 +90,29 @@ def _get_updated_bases(X1, X2, unnormed1, normed2, S1, k, eta1):
     # Take a gradient step on unnormed1
     unnormed1_next = unnormed1 - eta1 * gradient
 
-    # Take randomized SVD of quadratic of unnormed1 with coefficient matrix S1
-    unnormed1_quad_invert = get_svd_invert(quadratic(unnormed1_next, S1), random=False)
-
-    # Normalize unnormed 1 with inversion of quadratic
-    normed1 = np.dot(unnormed1_next, unnormed1_quad_invert)
+    # Normalize unnormed 1 with inversion of matrix quadratic
+    normed1 = _get_quadratic_normalized(unnormed1_next, S1)
 
     return (unnormed1_next, normed1)
 
-def _init_bases(p, p1, p2):
+def _get_init_bases(Sx, Sy):
+
+    p1 = Sx.shape[0]
+    p2 = Sy.shape[0]
+    p = min([p1,p2])
     
-    # Initialize Gaussian matrices for bases
-    Phi = randn(p1, p)
+    # Initialize Gaussian matrices for unnormalized bases
     unn_Phi = randn(p1, p)
-    Psi = randn(p2, p)
     unn_Psi = randn(p2, p)
 
+    # Normalize for initial normalized bases
+    Phi = _get_quadratic_normalized(unn_Phi, Sx)
+    Psi = _get_quadratic_normalized(unn_Psi, Sy) 
+
     return (Phi, unn_Phi, Psi, unn_Psi)
+
+def _get_quadratic_normalized(unnormed, S):
+
+    normalizer = get_svd_invert(quadratic(unnormed, S), random=False)
+
+    return np.dot(unnormed, normalizer)
