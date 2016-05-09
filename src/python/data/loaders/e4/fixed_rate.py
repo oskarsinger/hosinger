bee06b7c4cac5173c1e2f179d08911fbedd5d34b
oskarsinger <oskarsinger@gmail.com>
2016-05-08 21:32:19 -0400

    Updated e4 loader

--- src/python/data/loaders/e4/fixed_rate.py
+++ src/python/data/loaders/e4/fixed_rate.py
@@ -30,37 +30,54 @@ class FixedRateLoader(AbstractDataLoader):
         self.seconds = seconds
         self.window = self.hertz * self.seconds
         self.num_rounds = 0
-        self.data = None
+        self.data = self._get_file_data()
+        self.num_used_files = 1
 
     def get_datum(self):
 
-        if self.data is None:
-            data_list = []
-           
-            for (fp, ts) in self.timestamps:
-                with open(fp) as f:
-                    f.readline() # timestamp
-                    f.readline() # frequency
-                    data_list.extend([self.pl(line) for line in f])
+        self.num_rounds += 1
 
-            remainder = int(len(data_list) % self.window)
-            n = len(data_list) / self.window
+        # Is enough data in current file to fill a window?
+        if len(self.data) < self.window:
+            self.data = self._get_file_data()
+            self.num_used_files += 1
 
-            if remainder > 0:
-                data_list = data_list[:-remainder]
+        # Get the next full window
+        datum = self.data[:self.window]
+        
+        # Remove that window from data queue
+        self.data = self.data[self.window:]
 
-            data_array = np.array(data_list) 
-            self.data = np.reshape(data_array, (n, self.window))
+        return np.array(datum)
 
-        return self.data
+    def _get_file_data(self):
+
+        for (fp, ts) in self.timestamps:
+            with open(fp) as f:
+                f.readline() # timestamp
+                f.readline() # frequency
+
+            new_data = [self.pl(line) or line in f]
+
+            if len(new_data) < self.window:
+                raise ValueError(
+                    'File must have at least as many lines and hertz * seconds.')
+
+            yield new_data
 
     def get_status(self):
 
         return {
+            'dir_path': self.dir_path,
+            'filename': self.filename,
             'hertz': self.hertz,
             'seconds': self.seconds,
             'window': self.window,
-            'timestamps': self.filepaths
+            'timestamps': self.filepaths,
+            'num_rounds': self.num_rounds,
+            'num_used_files': self.num_used_files,
+            'process_line': self.pl,
+            'data': self.data
         }
 
     def cols(self):
