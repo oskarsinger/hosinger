7298de34b1bb08fbfbafd01728e1274d184057d8
oskarsinger <oskarsinger@gmail.com>
2016-07-02 17:39:16 -0400

    Fixed bug in drrobert random

--- src/python/drrobert/random.py
+++ src/python/drrobert/random.py
@@ -8,12 +8,20 @@ def rademacher(size=None, p=None):
 
     return np.random.choice(choices, size=size, p=p)
 
-def normal(loc=0.0, scale=1.0, shape=(1)):
+def normal(loc=0.0, scale=1.0, shape=1):
 
-    if len(shape) > 1:
+    size = shape
+
+    if type(shape) is tuple:
         size = prod(shape)
+    elif type(shape) is not int:
+        raise TypeError(
+            'Parameter shape must of type tuple or int.')
 
     vec = np.random.normal(
         loc=loc, scale=scale, size=size)
 
-    return np.reshape(vec, shape)
+    if type(shape) is tuple:
+        vec = np.reshape(vec, shape)
+
+    return vec
