9f8db871eb2e3c57d3b28f719d5c77a0ad79931c
oskarsinger <oskarsinger@gmail.com>
2016-07-04 14:24:57 -0400

    Added coordinate avging

--- src/python/data/servers/batch/batch.py
+++ src/python/data/servers/batch/batch.py
@@ -2,11 +2,16 @@ import numpy as np
 
 class BatchServer:
 
-    def __init__(self, data_loader, reg=0.1, lazy=True):
+    def __init__(self, 
+        data_loader, 
+        reg=0.1, 
+        lazy=True, 
+        num_coords=None):
 
         self.dl = data_loader
         self.lazy = lazy
         self.reg = reg
+        self.num_coords = num_coords
 
         self.data = None if lazy else self.dl.get_data()
 
@@ -15,7 +20,26 @@ class BatchServer:
         if self.data is None:
             self.data = self.dl.get_data()
 
+            if self.num_coords is not None:
+                self._avg_data()
+
         return np.copy(self.data)
+
+    def _get_avgd(self):
+
+        new_batch = np.zeros((self.data.shape[0], self.num_coords))
+        sample_size = self.cols() / self.num_coords
+
+        for i in xrange(self.num_coords):
+            begin = i * sample_size
+            end = begin + sample_size
+
+            if end + sample_size > batch.shape[1]+1:
+                new_batch[:,i] = np.mean(self.data[:,begin:], axis=1)
+            else:
+                new_batch[:,i] = np.mean(self.data[:,begin:end], axis=1)
+
+        return new_batch
 
     def cols(self):
 
