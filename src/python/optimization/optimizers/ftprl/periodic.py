b90618b5a0557ee2c3ebfdb33c9deedd6317f1cd
oskarsinger <oskarsinger@gmail.com>
2016-08-04 01:47:41 -0400

    Periodic mirror descent

--- src/python/optimization/optimizers/ftprl/periodic.py
+++ src/python/optimization/optimizers/ftprl/periodic.py
@@ -1,31 +1,52 @@
 from optimization.optimizers.ftprl import AbstractMatrixFTPRLOptimizer
+from drrobert.data_structures import FixedLengthQueue as FLQ
 
 import numpy as np
 
-class PeriodicMirrorDescent(AbstractMatrixFTPRLOptimizer):
+class PeriodicParameterMirrorDescent:
 
     def __init__(self,
-        period,
+        period, c,
         lower=None,
-        dual_avg=False,
         verbose=False):
 
         super(PeriodicMirrorDescent, self).__init__(lower, dual_avg)
 
         self.period = period
+        self.c = c
+        self.window = FLQ(self.period)
 
-    def _get_dual(self, primal):
+    def get_update(self, parameters, gradient, eta):
 
-        return primal
+        self.window.enqueue(np.copy(parameters))
 
-    def _get_primal(self, dual):
+        unscaled = parameters
 
-        return dual
+        if self.window.get_length() == self.period:
+            unscaled += self.c * self.window.dequeue()
+        elif self.window.get_length() > self.period:
+            raise Exception(
+                'PeriodicParameterMirrorDescent queue too long.')
+
+        unscaled -= eta * gradient
+
+        return (eta + eta * self.c)**(-1) * unscaled
 
     def get_status(self):
 
         status = super(MatrixAdaGrad, self).get_status()
 
         status['period'] = self.period
+        status['c'] = self.c
+        status['window'] = self.window
 
         return status
+
+class PeriodicSignalMirrorDescent:
+
+    def __init__(self,
+        period, c,
+        lower=None,
+        verbose=False):
+
+        print 'Stuff'
