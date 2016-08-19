522c497eedbcf5f120ff433538ff424ffecc4660
oskarsinger <oskarsinger@gmail.com>
2016-08-19 16:10:34 -0400

    Fixed init

--- src/python/optimization/optimizers/ftprl/__init__.py
+++ src/python/optimization/optimizers/ftprl/__init__.py
@@ -1,3 +1,4 @@
-from adagrad import AdaGradOptimizer
+from adagrad import DiagonalAdaGradOptimizer
+from adagrad import FullAdaGradOptimizer
 from matrix_mirror import SchattenPCOMIDOptimizer
 from periodic import PeriodicParameterProximalGradientOptimizer
