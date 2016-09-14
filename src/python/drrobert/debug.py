c45b82301ac48e9b22014e9593f4ed9721735bda
oskarsinger <oskarsinger@gmail.com>
2016-09-14 14:04:29 -0400

    Fixed bug maybe

--- src/python/drrobert/debug.py
+++ src/python/drrobert/debug.py
@@ -19,28 +19,32 @@ def handle_runtime_warning(func, exception_msg):
             raise Exception(exception_msg)
 
 def check_for_nan_or_inf(
-    np_array, 
+    variable, 
     loc_string, 
     var_name, 
     raise_error=True):
 
-    has_nan = np.any(np.isnan(np_array))
-    has_inf = np.any(np.isinf(np_array))
-    start = var_name + ' at ' + loc_string + ' has '
-    end = ' values inside.'
-    mk_msg = lambda x: start + x + end
-    msg = None
+    try:
+        has_nan = np.any(np.isnan(variable))
+        has_inf = np.any(np.isinf(variable))
+        start = var_name + ' at ' + loc_string + ' has '
+        end = ' values inside.'
+        mk_msg = lambda x: start + x + end
+        msg = None
 
-    if has_nan and has_inf:
-        msg = mk_msg('NaN and inf')
-    elif has_nan:
-        msg = mk_msg('NaN')
-    elif has_inf:
-        msg = mk_msg('inf')
-    
-    if msg is not None:
-        print msg
-        print str(np_array)
+        if has_nan and has_inf:
+            msg = mk_msg('NaN and inf')
+        elif has_nan:
+            msg = mk_msg('NaN')
+        elif has_inf:
+            msg = mk_msg('inf')
+        
+        if msg is not None:
+            print msg
+            print str(variable)
 
-        if raise_error:
-            raise ValueError(msg)
+            if raise_error:
+                raise ValueError(msg)
+    except TypeError:
+        pass
+
