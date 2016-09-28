9c20340b83a6aeed56a955460dffc699dcb58158
oskarsinger <oskarsinger@gmail.com>
2016-09-28 17:50:01 -0400

    Subsampling

--- src/python/runners/multiview/periodic.py
+++ src/python/runners/multiview/periodic.py
@@ -258,7 +258,8 @@ def _get_sampled_wavelets(Yh, Yl):
     basis = np.zeros((num_coeffs, len(hi_and_lo))) 
 
     for (i, y) in enumerate(hi_and_lo):
-        power = num_levels - i - 1
+        power = num_levels - i - 2
+        print y.shape
         basis[:,i] = np.copy(y[::2**power])
 
     return basis
