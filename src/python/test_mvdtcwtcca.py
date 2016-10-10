b38b3071c4b33d0c8a93e95cc670db9b1d04da39
oskarsinger <oskarsinger@gmail.com>
2016-10-10 01:59:29 -0400

    Stuff

--- src/python/test_mvdtcwtcca.py
+++ src/python/test_mvdtcwtcca.py
@@ -14,48 +14,70 @@ from bokeh.models.layouts import Column
     default='/home/oskar/Data/VirusGenomeData/FullE4/20160503_BIOCHRON_E4.hdf5')
 @click.option('--delay', default=None)
 @click.option('--save-load-dir', default='.')
-@click.option('--compute-wavelets', default=True)
+@click.option('--compute-wavelets', default=False)
 @click.option('--save-wavelets', default=False)
-@click.option('--compute-correlation', default=False)
-@click.option('--load-correlation', default=False)
-@click.option('--save-correlation', default=False)
-@click.option('--show-correlation', default=False)
-@click.option('--compute-cca', default=False)
-@click.option('--load-cca', default=False)
-@click.option('--save-cca', default=False)
-@click.option('--show-cca', default=False)
+@click.option('--compute-sp-wavelets', default=False)
+@click.option('--save-sp-wavelets', default=False)
+@click.option('--compute-sp-corr', default=False)
+@click.option('--load-sp-corr', default=False)
+@click.option('--save-sp-corr', default=False)
+@click.option('--show-sp-corr', default=False)
+@click.option('--compute-vpw-corr', default=False)
+@click.option('--load-vpw-corr', default=False)
+@click.option('--save-vpw-corr', default=False)
+@click.option('--show-vpw-corr', default=False)
+@click.option('--compute-dpw-corr', default=False)
+@click.option('--load-dpw-corr', default=False)
+@click.option('--save-dpw-corr', default=False)
+@click.option('--show-dpw-corr', default=False)
+@click.option('--compute-vpw-cca', default=False)
+@click.option('--load-vpw-cca', default=False)
+@click.option('--save-vpw-cca', default=False)
+@click.option('--show-vpw-cca', default=False)
 @click.option('--center', default=False)
 @click.option('--period', default=24*3600)
 @click.option('--subperiod', default=None)
 @click.option('--do-phase', default=False)
-@click.option('--correlation-kmeans', default=None)
-@click.option('--cca-kmeans', default=None)
+@click.option('--sp-corr-kmeans', default=None)
+@click.option('--vpw-corr-kmeans', default=None)
+@click.option('--vpw-cca-kmeans', default=None)
 @click.option('--show-kmeans', default=False)
-@click.option('--show-corr-subblocks', default=False)
-@click.option('--show-sp-correlation', default=False)
+@click.option('--show-vpw-corr-subblocks', default=False)
 def run_it_all_day(
     data_path, 
     delay,
     save_load_dir,
     compute_wavelets,
     save_wavelets,
-    compute_correlation,
-    load_correlation,
-    save_correlation, 
-    show_correlation,
-    compute_cca,
-    load_cca,
-    save_cca, 
-    show_cca,
+    load_wavelets,
+    compuet_sp_wavelets,
+    save_sp_wavelets,
+    load_sp_wavelets,
+    compute_sp_corr,
+    load_sp_corr,
+    save_sp_corr, 
+    show_sp_corr,
+    compute_vpw_corr,
+    load_vpw_corr,
+    save_vpw_corr, 
+    show_vpw_corr,
+    compute_dpw_corr,
+    load_dpw_corr,
+    save_dpw_corr, 
+    show_dpw_corr,
+    compute_vpw_cca,
+    load_vpw_cca,
+    save_vpw_cca, 
+    show_vpw_cca,
     center, 
     period,
     subperiod,
     do_phase,
-    correlation_kmeans,
-    cca_kmeans,
+    sp_corr_kmeans,
+    vpw_corr_kmeans,
+    vpw_cca_kmeans,
     show_kmeans,
-    show_corr_subblocks,
-    show_sp_correlation):
+    show_vpw_corr_subblocks)
 
     if correlation_kmeans is not None:
         correlation_kmeans = int(correlation_kmeans)
