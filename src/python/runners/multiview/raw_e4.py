b6c2611e1c8245d5f6a7c0dc04ff34c03d281150
oskarsinger <oskarsinger@gmail.com>
2016-10-10 22:21:55 -0400

    Stuff

--- src/python/runners/multiview/raw_e4.py
+++ src/python/runners/multiview/raw_e4.py
@@ -5,5 +5,14 @@ import data.loaders.e4.shortcuts as dles
 
 class E4RawDataPlotRunner:
 
-    def __init__(self,
-        hdf5_path):
+    def __init__(self, hdf5_path):
+
+        self.loaders = dles.get_e4_loaders_all_subjects(
+            hdf5_path, None, False)
+
+    def run(self):
+        print 'Poop'
+
+    def _get_averaged_data(self):
+
+        print 'Poop'
