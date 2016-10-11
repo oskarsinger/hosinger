2a344a6b0c649a973f97c07ade91e4679aefbb86
oskarsinger <oskarsinger@gmail.com>
2016-10-11 08:48:06 -0400

    Stuff

--- src/python/runners/multiview/sp_dtcwt.py
+++ src/python/runners/multiview/sp_dtcwt.py
@@ -239,7 +239,7 @@ class MVDTCWTSPRunner:
             current_data = [view[k*f:(k+1)*f]
                             for (f, view) in zip(factors, data)]
             sp_thresholds = [int(view.shape[0] * 1.0 / sp_f)
-                             for (view, f) in zip(data, sp_factors)]
+                             for (view, sp_f) in zip(data, sp_factors)]
             j = 0
             
             # Add a list to each view for the current super period
