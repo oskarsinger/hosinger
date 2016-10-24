545284fe80aa9c67a5eaf30320679e2226e9ce6f
oskarsinger <oskarsinger@gmail.com>
2016-10-24 13:57:21 -0400

    Stuff

--- src/python/runners/multiview/sp_dtcwt.py
+++ src/python/runners/multiview/sp_dtcwt.py
@@ -271,6 +271,9 @@ class MVDTCWTSPRunner:
                         self.biorthogonal,
                         self.qshift)
 
+                    print 'Yl right after computation', Yl
+                    print 'Yh right after computation', Yh
+
                     Yls[i][-1].append(Yl)
                     Yhs[i][-1].append(Yh)
 
