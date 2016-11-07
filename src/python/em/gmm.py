fa0fe32eb7ae5fc392fd7c6e24dd49bf5c204fdc
oskarsinger <oskarsinger@gmail.com>
2016-11-07 14:49:23 -0500

    Stuff

--- src/python/em/gmm.py
+++ src/python/em/gmm.py
@@ -3,14 +3,18 @@ import numpy as np
 from optimization.optimizers import GradientOptimizer as GO
 from optimization.stepsize import FixedScheduler as FXS
 
-class OnlineUnivariateGaussianMixtureModel:
+# TODO: account for baseline mu and sigma in E and M steps 
+class OnlineUnivariateRademacherGaussianMixtureModel:
 
     def __init__(self, 
-        K=2,
+        baseline_mu=0,
+        baseline_sigma=0,
         optimizer=None, 
         eta_scheduler=None):
 
-        self.K = K
+        self.baseline_mu = baseline_mu
+        self.baseline_sigma = baseline_sigma
+        self.K = 2
 
         if optimizer is None:
             self.optimizer = GO()
@@ -28,17 +32,25 @@ class OnlineUnivariateGaussianMixtureModel:
         self.sigmas = np.ones(self.K)
         self.num_rounds = 0
 
-    def get_update(self, data):
+    def get_update(self, sample, r_scale):
 
         self.num_rounds += 1
 
-        self._compute_E_step(data)
+        self._compute_E_step(data, r_scale)
+        self._compute_M_step()
         
-    def _get_E_step(self, data):
+    def _get_E_step(self, data, r_scale):
 
         densities = self._get_densities(data)
         numers = densities * self.ps
-        numers / np.sum(numers)
+        conditional_ps = numers / np.sum(numers)
+        s_bar = None
+        s_current = None
+        eta = self.eta_scheduler.get_stepsize()
+        search_direction = s_bar - s_current
+        # TODO: Make sure negating search_direction is equivalent to gradient ascent
+        s_new = self.optimizer.get_update(
+            s_current, -search_direction, eta)
 
     def _get_densities(self, data):
 
