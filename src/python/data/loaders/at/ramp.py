338191ce400f749de356d8cc5197ed05059a468b
oskarsinger <oskarsinger@gmail.com>
2016-12-07 10:19:48 -0500

    STUFF

--- src/python/data/loaders/at/ramp.py
+++ src/python/data/loaders/at/ramp.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from math import floor
+from math import floor, ceil
 
 class AlTestRampLoader:
 
@@ -11,6 +11,7 @@ class AlTestRampLoader:
         prop_jitter=0.0,
         supp1=0.1, supp2=0.2,
         sigma_s1=0.5, sigma_s2=0.5,
+        sigma_n=0.001,
         s1=6, s2=6,
         random_scaling=True):
 
@@ -22,6 +23,7 @@ class AlTestRampLoader:
         self.supp2 = supp2
         self.sigma_s1 = sigma_s1
         self.sigma_s2 = sigma_s2
+        self.sigma_n = sigma_n
         self.s1 = s1
         self.s2 = s2
         self.random_scaling = random_scaling
@@ -33,16 +35,20 @@ class AlTestRampLoader:
             (np.random.uniform((N,)) - 0.5) * \
             self.prop_jitter * \
             self.T)
-        signal1 = 'Poop'
-        signal2 = 'Poop'
+        signal1 = np.arange(
+            int(self.T * self.supp1))[:,np.newaxis]
+        signal1 /= np.linalg.norm(signal1)
+        signal2 = np.ones(
+            (int(self.T * self.supp2), 1))
+        signal2 /= np.linalg.norm(signal2)
 
-        self.cycle1 = self._get_cycle(
+        self.TS1 = self._get_TS(
             signal1,
             self.sigma_s1,
             self.s1,
             self.supp1,
             rand_offsets)
-        self.cycle2 = self._get_cycle()
+        self.TS2 = self._get_TS(
             signal2,
             self.sigma_s2,
             self.s2,
@@ -51,7 +57,7 @@ class AlTestRampLoader:
 
     def get_data(self):
 
-        return (self.cycle1, self.cycle2)
+        return (self.TS1, self.TS2)
 
     def _get_cycle(self, 
         signal, 
@@ -60,11 +66,30 @@ class AlTestRampLoader:
         supp, 
         rand_offsets):
 
-        cycle = None
+        periods = []
 
         for k in xrange(self.N):
 
+            ro = random_offsets[k]
+            zs = np.zeros(
+                (int(ceil(self.T * (1-supp))+ro),1))
+            period = np.copy(signal)
+
             if self.random_scaling:
-                cycle = 'Poop'
-            else:
-                cycle = 'Poop'
+                scaling = sigma * np.abs(np.random.randn())
+                period = scaling * period
+
+            periods.extend([period, zs])
+
+        full = np.vstack(periods)
+
+        if full.shape[0] > self.num_points:
+            full = full[:self.num_points,:]
+        else:
+            padding = np.zeros((
+                self.num_points - full.shape[0],1))
+            full = np.hstack([full, padding])
+
+        noise = np.random.randn(self.num_points, 1)
+
+        return cycle + self.sigma_n * noise
