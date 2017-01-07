354c0a1a87893a460038460527ed5a602df5c562
oskar <oskarsinger@gmail.com>
2017-01-07 15:07:51 -0500

    STUFF

--- src/python/run_raw_e4.py
+++ src/python/run_raw_e4.py
@@ -8,19 +8,22 @@ from runners.wavelets import E4RawDataPlotRunner as E4RDPR
 @click.option('--missing', default=False)
 @click.option('--complete', default=False)
 @click.option('--std', default=False)
+@click.option('--avg-over-subjects', default=False)
 def run_things_all_day_bb(
     data_path,
     period,
     missing,
     complete,
-    std):
+    std,
+    avg_over_subjects):
 
     runner = E4RDPR(
         data_path,
         period=period,
         missing=missing,
         complete=complete,
-        std=std)
+        std=std,
+        avg_over_subjects=avg_over_subjects)
 
     runner.run()
 
