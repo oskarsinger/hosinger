098a790f3021acd7844d1fa24ada8b06966514ef
oskar <oskarsinger@gmail.com>
2017-01-11 17:25:07 -0500

    STUFF

--- src/python/wavelets/dtcwt/oned.py
+++ src/python/wavelets/dtcwt/oned.py
@@ -18,8 +18,6 @@ def dtwavexfm(
     L = X.shape
 
     if L[0] % 2 > 0:
-        print 'Necessary to extend by one nan'
-        print 'L', L
         X = np.vstack([
             X,
             np.ones((1,1)) * np.nan])
