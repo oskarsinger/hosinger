a7236a3f213afe9f8bf4f3b6328294903c313942
oskar <oskarsinger@gmail.com>
2017-01-21 16:38:45 -0500

    STUFF

--- src/python/data/servers/masks/__init__.py
+++ src/python/data/servers/masks/__init__.py
@@ -2,3 +2,4 @@ from periodic import PeriodicMask
 from percentile import PercentileMask
 from threshold import ThresholdEventMask
 from wavelet import WaveletMask
+from interpolate import Interp1DMask
