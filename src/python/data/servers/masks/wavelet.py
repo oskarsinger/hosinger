4cbe27cdbc967a935164ce7a01d869b723d4e9c4
oskar <oskarsinger@gmail.com>
2017-01-23 14:17:32 -0500

    STUFF

--- src/python/data/servers/masks/wavelet.py
+++ src/python/data/servers/masks/wavelet.py
@@ -5,6 +5,7 @@ import numpy as np
 import wavelets.dtcwt as dtcwt
 
 from drrobert.file_io import get_timestamped as get_ts
+from wavelets.dtcwt.utils import get_padded_wavelets as get_pw
 from wavelets.dtcwt.oned import get_partial_reconstructions as get_pr
 from linal.utils.misc import get_array_mod
 
@@ -15,9 +16,11 @@ class WaveletMask:
         period,
         max_freqs,
         save_load_path,
+        padded=False,
+        pr=False,
+        serve_one_period=False,
         load=False,
-        save=False,
-        get_pr=None):
+        save=False):
 
         # For now, assume server is batch-style
         self.ds = ds
@@ -26,19 +29,30 @@ class WaveletMask:
         self.hertz = self.ds.get_status()['data_loader'].get_status()['hertz']
         self.max_freqs = max_freqs
         self.save_load_path = save_load_path
-        self.get_pr = get_pr
+        self.padded = padded
+        self.pr = pr
+        self.serve_one_period = serve_one_period
         self.load = load
         self.save = save
 
-        self.pr = self.get_pr is not None
         self.window = int(self.period * self.hertz)
         self.num_freqs = min([
             int(log(self.window, 2)) - 1,
             self.max_freqs])
+        self.num_rounds = 0
         self.biorthogonal = wdtcwt.utils.get_wavelet_basis(
             'near_sym_b')
         self.qshift = wdtcwt.utils.get_wavelet_basis(
             'qshift_b')
+        self.data = None
+
+        if not self.load:
+            data = self.ds.get_data()
+            num_rows = int(float(data.shape[0]) / self.window)
+            reshaped = np.reshape(
+                get_array_mod(data, self.window),
+                (num_rows, self.window))
+            self.data = reshaped
         
         hdf5_repo = None
 
@@ -64,13 +78,69 @@ class WaveletMask:
 
     def get_data(self):
 
+        wavelets = None
+
+        if self.serve_one_period:
+            wavelets = self._get_one_period(self.num_rounds)
+
+            self.num_rounds += 1
+        else:
+            num_rounds = len(self.hdf5_repo) \
+                if self.load else \
+                self.data.shape[0]
+            wavelets = [self._get_one_period(i)
+                        for i in xrange(num_rounds)]
+
+        return wavelets
+
+    def _get_one_period(self, i):
+
+        wavelets = None
+
+        if self.load:
+            group = self.hdf5_repo[str(i)]
+            num_Yh = len(group) - 1
+            Yh = [np.array(group['Yh_' + str(j)]) 
+                  for j in xrange(num_Yh)]
+            Yl = np.array(group['Yl'])
+            wavelets = (Yh, Yl)
+        else:
+            key = str(i)
+            (Yl, Yh, _) = dtcwt.oned.dtwavexfm(
+                self.data[i,:][:,np.newaxis],
+                num_freqs - 1,
+                self.biorthogonal,
+                self.qshift)
+            wavelets = (Yh, Yl)
+
+            if self.save:
+                self.hdf5_repo.create_group(key)
+                group = self.hdf5_repo[key]
+
+                for (j, freq) in enumerate(Yh):
+                    group[key].create_dataset(
+                        'Yh_' + str(j), freq)
+
+                group[key].create_dataset(
+                    'Yl', freq)
+
+        if self.padded:
+            wavelets = get_pw(Yh, Yl)
+        elif self.pr:
+            wavelets = get_pr(Yh, Yl, self.biorthogonal, self.qshift)
+
+        return wavelets
+
+    def _get_all_periods(self):
+
         wavelets = []
 
         if self.load:
             for i in xrange(len(self.hdf5_repo)):
                 group = self.hdf5_repo[str(i)]
                 num_Yh = len(group) - 1
-                Yh = [np.array(group['Yh_' + str(j)]) for j in xrange(num_Yh)]
+                Yh = [np.array(group['Yh_' + str(j)]) 
+                      for j in xrange(num_Yh)]
                 Yl = np.array(group['Yl'])
 
                 wavelets.insert((Yh, Yl))
@@ -102,8 +172,11 @@ class WaveletMask:
                     group[key].create_dataset(
                         'Yl', freq)
 
-        if self.pr:
-            wavelets = [get_pr(Yh, Yl) for (Yh, Yl) in wavelets]
+        if self.padded:
+            wavelets = [get_pw(Yh, Yl) for (Yh, Yl) in wavelets]
+        elif self.pr:
+            wavelets = [get_pr(Yh, Yl, self.biorthogonal, self.qshift)
+                        for (Yh, Yl) in wavelets]
 
         return wavelets
 
@@ -127,7 +200,8 @@ class WaveletMask:
             'hertz': self.hertz,
             'max_freqs': self.max_freqs,
             'save_load_path': self.save_load_path,
-            'pr': self.get_pr,
+            'padded': self.padded,
+            'pr': self.pr,
             'load': self.load,
             'save': self.save,
             'window': self.window,
--- src/python/wavelets/dtcwt/utils.py
+++ src/python/wavelets/dtcwt/utils.py
@@ -43,3 +43,45 @@ def get_wavelet_basis(wavelet_name):
 
     return {t : np.array(vec)[:,np.newaxis]
             for (t, vec) in val_lists.items()}
+
+def get_padded_wavelets(Yh, Yl):
+
+    hi_and_lo = Yh + [Yl]
+    num_rows = hi_and_lo[0].shape[0]
+    basis = np.zeros(
+        (num_rows, len(hi_and_lo)),
+        dtype=complex)
+    basis[:,0] = np.copy(hi_and_lo[0][:,0])
+
+    for (i, y) in enumerate(hi_and_lo[1:]):
+        power = i + 1
+
+        for j in xrange(power):
+            max_len = basis[j::2**power,power].shape[0]
+            basis[j::2**power,power] = np.copy(y[:max_len,0])
+
+    return basis
+
+def get_sampled_wavelets(Yh, Yl):
+
+    hi_and_lo = Yh + [Yl]
+    k = None
+
+    for (i, y) in enumerate(hi_and_lo):
+        if y.shape[0] > i:
+            k = i+1
+        else:
+            break
+
+    hi_and_lo = hi_and_lo[:k]
+    num_rows = hi_and_lo[-1].shape[0]
+    basis = np.zeros(
+        (num_rows, k),
+        dtype=complex)
+
+    for (i, y) in enumerate(hi_and_lo):
+        power = k - i - 1
+        sample = np.copy(y[::2**power,0])
+        basis[:,i] = sample
+
+    return basis
