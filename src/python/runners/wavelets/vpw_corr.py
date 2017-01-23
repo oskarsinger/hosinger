e0064f7629c5c28d4d6b9202d5397e757f2dfec2
oskar <oskarsinger@gmail.com>
2017-01-22 23:21:45 -0500

    STUFF

--- src/python/runners/wavelets/vpw_corr.py
+++ src/python/runners/wavelets/vpw_corr.py
@@ -234,7 +234,7 @@ class ViewPairwiseCorrelationRunner:
                 # TODO: add frame to indicate end of 24-hour period
 
                 for (sp, corr) in enumerate(subperiods):
-                    plot = get_plot(corr, fig.axes, sp, p)
+                    plot = get_plot(corr, fig.axes[-1], sp, p)
 
                     writer.grab_frame()
 
