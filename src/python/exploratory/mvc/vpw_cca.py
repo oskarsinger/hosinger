c5950ababa0ba4816e9ae07c11cd8a038c7436a7
oskar <oskarsinger@gmail.com>
2017-01-31 15:42:19 -0500

    STUFF

--- src/python/exploratory/mvc/vpw_cca.py
+++ src/python/exploratory/mvc/vpw_cca.py
@@ -375,7 +375,7 @@ class ViewPairwiseCCA:
                 filename = '_'.join([
                     'subject', s,
                     '_views_',
-                    self.names[v1] + '-' + self.names[v2]]) + '.png'
+                    self.names[s][v1] + '-' + self.names[s][v2]]) + '.png'
                 path = os.path.join(
                     self.n_time_p_frequency_dir, filename)
                 (v1_l, v2_l) = unzip(
@@ -383,7 +383,7 @@ class ViewPairwiseCCA:
                 v1_tl = np.hstack(v1_l)
                 v2_tl = np.hstack(v2_l)
                 title = 'View-pairwise cca (n time p frequency) for views ' + \
-                    self.names[v1] + ' ' + self.names[v2] + \
+                    self.names[s][v1] + ' ' + self.names[s][v2] + \
                     ' of subject ' + s + ' at subperiod ' + str(sp)
                 x_name = 'subperiod'
                 y_name = 'dimension'
