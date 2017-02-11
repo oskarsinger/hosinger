fa3b2c9eb34aef2ecb6c27d50ea5cc33f02f038b
oskar <oskarsinger@gmail.com>
2017-02-11 01:09:06 -0500

    STUFF

--- src/python/drrobert/stats.py
+++ src/python/drrobert/stats.py
@@ -3,8 +3,9 @@ import spancca
 import numpy as np
 
 from scipy.stats import pearsonr
-from sklearn.cross_decomposition import CCA
-from linal.utils.misc import get_safe_power
+from sklearn.decomposition import TruncatedSVD
+from linal.utils.misc import get_safe_power,
+from linal.svd_funcs import get_svd_power
 
 def get_pearson_matrix(X1, X2):
 
@@ -24,26 +25,53 @@ def get_pearson_matrix(X1, X2):
     masked_X2 = np.ma.masked_invalid(zm_X2)
 
     numerator = np.dot(
-        masked_X1.T, masked_X2).filled(0) / n
+        masked_X1.H, masked_X2).filled(0) / n
     denominator = np.dot(
             std1[:,np.newaxis], 
-            std2[:,np.newaxis].T)
+            std2[:,np.newaxis].H)
     inv_denom = get_safe_power(
         denominator, -1)
 
     return numerator * inv_denom
 
-def get_cca_vecs(X1, X2, num_nonzero=None):
+def get_cca_vecs(X1, X2, n_components=1, num_nonzero=None):
 
-    (x_weights, y_weights) = [None] * 2
+    (n1, p1) = X1.shape
+    (n2, p2) = X2.shape
+    n = None
+
+    if not n1 == n2:
+        raise ValueError(
+            'X1 and X2 must have same 1st dimension.')
+    else:
+        n = n1
+
+    (x1_weights, x2_weights) = [None] * 2
 
     if num_nonzero is None:
-        cca = CCA(n_components=1)
+        # Get means and zero-mean vars
+        mu1 = np.nanmean(X1, axis=0)
+        mu2 = np.nanmean(X2, axis=0)
+        zm_X1 = X1 - mu1
+        zm_X2 = X2 - mu2
 
-        cca.fit(X1, X2)
+        # Get sample covariance matrices
+        CX1 = np.dot(zm_X1.H, zm_X1) / n
+        CX2 = np.dot(zm_X2.H, zm_X2) / n
+        CX12 = np.dot(zm_X1.H, zm_X2) / n
 
-        x_weights = cca.x_weights_
-        y_weights = cca.y_weights_
+        # Get inverse sqrts and normed sample cross-covariance
+        CX1_inv_sqrt = get_svd_power(CX1, -0.5)
+        CX2_inv_sqrt = get_svd_power(CX2, -0.5)
+        Omega = np.dot(
+            CX1_inv_sqrt,
+            np.dot(CX12, CX2_inv_sqrt))
+
+        # Get canonical vectors
+        (U, s, V) = np.linalg.svd(Omega)
+        (x1_weights, x2_weights) = (
+            np.dot(CX1_inv_sqrt, U[:,0]), 
+            np.dot(CX2_inv_sqrt, V.H[:,0]))
     else:
         x_project = spancca.projections.setup_sparse(
             nnz=num_nonzero)
@@ -63,4 +91,3 @@ def get_cca_vecs(X1, X2, num_nonzero=None):
     return (
         x_weights,
         y_weights)
-
