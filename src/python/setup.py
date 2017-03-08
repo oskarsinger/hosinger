764332950bbfe1276a69b5b71417155326d2b897
oskar <oskarsinger@gmail.com>
2017-03-08 15:35:47 -0500

    Rm'd bad subpkgs from setup.py

--- src/python/setup.py
+++ src/python/setup.py
@@ -5,11 +5,9 @@ setup(
     version='0.01',
     packages=[
         'linal',
-        'linal.tensor',
         'linal.random',
         'linal.utils',
         'lazyprojector',
         'drrobert',
         'drrobert.data_structures',
-        'drrobert.fp',
         'models'])
