38b1ba0f8e79ee6870d9c6038019088780f98fc7
oskar <oskarsinger@gmail.com>
2017-04-06 13:31:04 -0400

    Changed func name

--- src/python/linal/utils/inverse.py
+++ src/python/linal/utils/inverse.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from .products import get_multi_dot as gmd
 
-def get_rank1_inv_update(A_inv, b, c):
+def get_sherman_morrison(A_inv, b, c):
     # Sherman-Morrison update from Matrix Cookbook
 
     numerator = np.dot(
