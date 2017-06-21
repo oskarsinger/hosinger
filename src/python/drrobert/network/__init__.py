93437bd8869813ee6198a3a22033e446b929b8ae
oskar <oskarsinger@gmail.com>
2017-06-21 17:36:56 -0400

    Structural changes

--- src/python/drrobert/network/__init__.py
+++ src/python/drrobert/network/__init__.py
@@ -2,3 +2,4 @@ from .generate import *
 from .misc import *
 
 from . import subset
+from . import spectral
---
+++ src/python/drrobert/network/spectral/param.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+from linal.utils import get_quadratic as gq
+from drrobert.network import get_random_parameter_graph as get_rpg
+
+class L2RegularizedRandomParameterGraph:
+
+    def __init__(self, N, p, fc=True, dist=True, sym=True):
+        # TODO: actually use the symmetric graph option
+
+        self.N = N
+        self.p = p
+
+        (self.ws, self.Bw, self.Dw, self.G) = get_rpg(
+            self.N, 
+            self.p, 
+            fc=fc,
+            dist=dist)
+        self.d = np.sum(self.G, axis=1)
+        self.L = np.diag(d) - self.G
+
+        (self.e_vals, self.e_vecs) = np.linalg.eig(self.L)
+
+    def get_logdet(self, loc, scale):
+
+        log_e_vals = np.log(scale * self.e_vals + loc)
+
+        return np.sum(log_e_vals) * p
+
+    def get_L_and_inv(self, loc, scale):
+
+        aug_L_e_vals = scale * self.e_vals + loc
+        lambda_matrix = np.diag(aug_L_e_vals)
+        lambda_matrix_inv = np.diag(
+            np.power(aug_L_e_vals, -1))
+
+        aug_L = gq(self.e_vecs, lambda_matrix)
+        aug_L_inv = gq(self.e_vecs, lambda_matrix_inv)
+
+        return (aug_L, aug_L_inv)
