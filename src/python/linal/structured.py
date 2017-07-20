070a449f0f52136379a37871a234cecfd6bdb164
oskar <oskarsinger@gmail.com>
2017-07-19 23:16:40 -0400

    Restructuring

--- src/python/linal/structured.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-from scipy.linalg import hadamard
-
-def get_normed_hadamard(n):
-
-    return n**(-0.5) * hadamard(n)
--- src/python/linal/utils/inverse.py
+++ src/python/linal/utils/inverse.py
@@ -1,6 +1,20 @@
 import numpy as np
 
 from .products import get_multi_dot as gmd
+from .svd_funcs import get_svd_power
+from .utils import get_quadratic as gq
+
+def get_woodbury_inversion(H, rho):
+
+    (m, d) = H.shape
+    I_m = np.identity(m)
+    I_d = np.identity(d)
+    to_invert = rho*I_m + np.dot(H, H.T)
+    inversion = get_svd_power(to_invert, -1)
+    quad = gq(H, inversion)
+    unscaled = I_d - quad
+
+    return rho**(-1) * unscaled
 
 def get_sherman_morrison(A_inv, b, c):
     # Sherman-Morrison update from Matrix Cookbook
